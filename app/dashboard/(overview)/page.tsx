07c5039171eef456dc6ab28ee53c8c1b4c5827c6
Viroun KATTYGNARATH <virounkattygnarath@OrdinatdeViroun.lan>
2024-12-05 13:03:56 +0100

    finished tutorial

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,37 @@
+import { CardsSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+import { Suspense } from "react";
+import CardWrapper from "../../ui/dashboard/cards";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { lusitana } from "../../ui/fonts";
+import { LatestInvoicesSkeleton } from "../../ui/skeletons";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+const Page = async () => {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
